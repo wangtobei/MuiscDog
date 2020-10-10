88d9dde86170566f34591eb7cd95961b030e47d3
wangbeibei <1498580056@qq.com>
2020-10-10 12:08:16 +0800

    10.10

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -17,8 +17,13 @@ export default (state = DefaultStore, action) => {
       NewStore.CurrentMusicPic = action.CurrentMusicPic;
       return NewStore;
     case "AddMusicToList": //添加新的音乐 且当前播放的音乐就是最新的
-      NewStore.SongList.push(action.MusicId);
-      NewStore.SongIndex = NewStore.SongList.length - 1;
+      let index = state.SongList.indexOf(action.MusicId);
+      if (index === -1) {
+        NewStore.SongList.push(action.MusicId);
+        NewStore.SongIndex = NewStore.SongList.length - 1;
+      } else {
+        NewStore.SongIndex = index;
+      }
       return NewStore;
     case "ChangeSongIndex":
       NewStore.SongIndex = action.SongIndex;
